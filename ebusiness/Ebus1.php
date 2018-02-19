b60d62bda77570a06a22e7314f95e02ce0950241
David Keane <117386211@umail.ucc.ie>
2018-02-19 21:03:41 +0000

    first commit

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -19,34 +19,37 @@
 <br/>
 
     <form method = "POST" action ="Ebus2.php">
-<label for "salesforce"> </label>
-<input type = "radio" id = "Salesforce" name = "product" onClick = "disabledbtnProceed"
+<label for "salesforce"> 
+<input type = "radio" id = "Salesforce" name = "product" onClick = "disabledbtnProceed"/>
 AWS @$300
-
+</label>
 
 <br/>
 <br/>
 
-<lable for "Subtotal">
+<label for "Subtotal">
 Sub Total
 <input type = "text" id = "Subtotal" value = "0.00" readonly/>
 </label>
 
 <br/>
+<br/>
 
-<lable for "Total">
-Sub Total
+<label for "Total">
+Total
 <input type = "text" id = "Total" value = "0.00" readonly/>
 </label>
         
         <br/>
+        <br/>
+        
 <button type "submit" id = "btnProceed" disable>Add to Shopping Cart </button>
 </form>
 
 <br/>
 
 <button onClick="calcSub()"Calculate Cost </button>
-<a role = "button" href = "Ebus1.php"> clear Choice </a>
+<a role = "button" href = "Ebus1.php"> Clear Choice </a>
 
 </body>
 </html>
