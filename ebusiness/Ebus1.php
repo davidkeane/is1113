a35afecdd7b9122e3855bcd6749f562242273e76
David Keane <117386211@umail.ucc.ie>
2018-03-06 01:02:14 +0000

    push to github

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -33,9 +33,41 @@ SalesForce @ $100
 <br/>
 <br/>
 
+<label for "cloud9"> 
+<input type = "radio" id = "cloud9" name = "product" onClick = "disabledbtnProceed()"/>
+Cloud 9 @ $200
+</label>
+
+<br/>
+<br/>
+
+<label for "gmail"> 
+<input type = "radio" id = "gmail" name = "product" onClick = "disabledbtnProceed()"/>
+SalesForce @ $400
+</label>
+
+<br/>
+<br/>
+
 <label for="subtotal">
 Sub Total
 <input type = "text" id = "subtotal" value = "0.00" readonly/>
+</label>
+
+<br/>
+<br/>
+
+<label for="discount">
+Discount
+<input type = "text" id = "discount" value = "0.00" readonly/>
+</label>
+
+<br/>
+<br/>
+
+<label for="vat">
+V.A.T.
+<input type = "text" id = "vat" value = "0.00" readonly/>
 </label>
 
 <br/>
--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -2,23 +2,51 @@
 
 function calcSub(){
     
-    
     var argSubTotal;
     
     if(document.getElementById('salesforce').checked){
     argSubTotal = 100;
 }
-else{
+else if(document.getElementById('aws').checked){
     argSubTotal = 300;
 }
 
-display(argSubTotal);
+else if(document.getElementById('cloud9').checked){
+    argSubTotal = 200;
 }
 
-function display(parm1){
+else if(document.getElementById('gmail').checked){
+    argSubTotal = 400;
+}
+
+display(argSubTotal);
+calcDisVatTotal(argSubTotal);
+}
+
+function calcDisVatTotal(parmSubTotal){
+    
+    var SubTotal;
+    var discountAmt;
+    var vatAmt;
+    var totalAmt;
+    
+    SubTotal = parmSubTotal;
+    
+    vatAmt = (SubTotal - discountAmt) * 0.1;
+    
+    totalAmt = (SubTotal - discountAmt) + vatAmt;
+    
+    display(SubTotal, discountAmt, vatAmt, totalAmt);
+    
+}
+
+function display(parm1, parm2, parm3, parm4){
     
     document.getElementById("subtotal").value = parm1;
-    document.getElementById("total").value = parm1;
+    document.getElementById("discount").value = parm2;
+    document.getElementById("vat").value = parm3;
+    document.getElementById("total").value = parm4;
+    
     
     enablebtnProceed();
 }
