77d383f2ddfa64ad0a53569efad15397b8f7f730
David Keane <117386211@umail.ucc.ie>
2018-03-09 11:31:45 +0000

    push

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -116,15 +116,6 @@ input{
          
     </body>
     
-      <footer>
-        <a href="https://ucc.ie">
-            <img position ="static" src="https://www.cubsucc.com/images/layout/uccLogo.png"/>
-        </a>
-        <a id="cubs" href="https://www.cubsucc.com/">
-            <img position = "absolute" src="https://www.cubsucc.com/images/layout/logo.png"/>
-        </a>
-    </footer>
-    
 </html>
 
 
